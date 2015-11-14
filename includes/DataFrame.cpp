ac66e8b09e9f6618f1154d947ba1e661f8379fc5
Daniel Sorichetti <me@dancodes.com.ar>
2015-11-14 00:47:08 -0300

    Agregado el calculo de ganancia de informacion del Police District. muy importante implementar esto para los valores numericos

--- includes/DataFrame.cpp
+++ includes/DataFrame.cpp
@@ -8,14 +8,18 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
-
+#include <map>
+#include <math.h>
 
 using namespace std;
 
 DataFrame::DataFrame() {
-    this->crimenes = std::vector<Crimen*>();
-    this->crimenes.reserve(25000);
+    this->crimenes = new std::vector<Crimen*>();
+    this->crimenes->reserve(25000);
+}
+
+DataFrame::DataFrame(std::vector<Crimen*>* crimenes_previos) {
+    this->crimenes = crimenes_previos;
 }
 
 void DataFrame::guardarEnDisco() {
@@ -28,7 +32,7 @@ void DataFrame::leerArchivo() {
 
     io::CSVReader<9, io::trim_chars<' '>, io::double_quote_escape<',','\"'>> in("data_pruebas/train.25000.csv");
 
-    in.read_header(io::ignore_extra_column, "Dates","Category","Descript","DayOfWeek","PdDistrict","Resolution","Address","X","Y");
+    in.read_header(io::ignore_extra_column,"Dates","Category","Descript","DayOfWeek","PdDistrict","Resolution","Address","X","Y");
 
     string Dates;
     string Category;
@@ -44,26 +48,81 @@ void DataFrame::leerArchivo() {
 
     unsigned int c = 0;
 
+    std::vector<Crimen*>::iterator it;
+
+    it = this->crimenes->begin();
+
     while(in.read_row(Dates,Category,Descript,DayOfWeek,PdDistrict,Resolution,Address,X,Y)){
 
         Crimen* crimen = new Crimen(X,Y,PdDistrict);
 
-        this->crimenes[c] = crimen;
+        this->crimenes->insert(it,crimen);
+
         c++;
 
         //cout << crimen->obtenerX() << "," << crimen->obtenerY() << "," << *crimen->obtenerPd() << endl;
     }
 
     cout << "Leidos " << c+1 << " crimenes." << endl;
-    for(std::vector<int>::size_type i = 0; i < 5; i++) {
-        Crimen* crimen = this->crimenes[i];
+
+    //Loop para mostrar los top 5 en la pantalla
+
+    for(std::vector<int>::size_type i = 0; i < 3; i++) {
+        Crimen* crimen = (*this->crimenes)[i];
         cout << crimen->obtenerX() << "\t" << crimen->obtenerY() << "\t" << *crimen->obtenerPd() << endl;
     }
+
+
+    cout << "Ganancia de Información de PdDistrict: " << this->infoGainPd() << endl;
+}
+
+unsigned int DataFrame::cantidad() {
+    return this->crimenes->size();
+}
+
+double DataFrame::infoGainPd() {
+
+
+    std::map<string, unsigned int> frequencia_de_clase = std::map<string, unsigned int>();
+
+    for(std::vector<int>::size_type i = 0; i < this->crimenes->size(); i++) {
+
+        Crimen* actual = (*this->crimenes)[i];
+        std::string pd_actual = *actual->obtenerPd();
+
+        if(frequencia_de_clase.count(pd_actual) == 0) {
+
+            frequencia_de_clase[pd_actual] = 1;
+
+        } else {
+
+            int freq_actual = frequencia_de_clase[pd_actual];
+            frequencia_de_clase[pd_actual] = freq_actual + 1;
+        }
+    }
+
+    double info_gain = 0;
+
+    // show content:
+    for (std::map<string, unsigned int>::iterator it=frequencia_de_clase.begin(); it!=frequencia_de_clase.end(); ++it) {
+        unsigned int freq_ci = it->second;
+
+        info_gain = info_gain +
+        (((double)freq_ci/(double)this->cantidad()) *
+        log2((double)freq_ci/(double)this->cantidad()));
+
+    }
+
+    info_gain = info_gain * (-1.0);
+
+
+    return info_gain;
 }
 
 DataFrame::~DataFrame() {
-    for(std::vector<int>::size_type i = 0; i != this->crimenes.size(); i++) {
-        delete this->crimenes[i];
+    for(std::vector<int>::size_type i = 0; i != this->crimenes->size(); i++) {
+        delete (*this->crimenes)[i];
     }
 
+    delete this->crimenes;
 }
--- includes/DataFrame.hpp
+++ includes/DataFrame.hpp
@@ -9,10 +9,12 @@ using namespace std;
 class DataFrame {
 public:
     DataFrame();
+    DataFrame(std::vector<Crimen*>* crimenes_previos);
     void guardarEnDisco();
     void leerArchivo();
+    unsigned int cantidad();
+    double infoGainPd();
     ~DataFrame();
 private:
-    std::vector<Crimen*> crimenes;
-    
+    std::vector<Crimen*>* crimenes;
 };
