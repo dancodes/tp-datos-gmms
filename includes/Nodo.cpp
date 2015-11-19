0cc22511f90379d5d3a5ace71c1ec189a41cdedc
nachomaness <nachi.maness@gmail.com>
2015-11-19 14:11:18 -0300

    le agrego los df y el constructor a la clase nodo

--- includes/Nodo.cpp
+++ includes/Nodo.cpp
@@ -2,7 +2,16 @@
 
 Nodo::Nodo()
 {
-    //ctor
+    this->atributo = " ";
+    this->categoria = " ";
+    this->infoGain = 0.0;
+}
+
+Nodo::Nodo(DataFrame* df,std::string atrib,std::string cat,double IG){
+    this->dataFrame = df;
+    this->atributo = atrib;
+    this->categoria = cat;
+    this->infoGain = IG;
 }
 
 void Nodo::AgregarNodo(Nodo* nodoAAgregar){
--- includes/Nodo.hpp
+++ includes/Nodo.hpp
@@ -4,13 +4,14 @@ class Nodo
 {
     public:
         Nodo();
+        Nodo(DataFrame* df,std::string atrib,std::string cat,double IG);
         void AgregarNodo(Nodo* nodoAAgregar);
         Nodo* ObtenerNodo();
         ~Nodo();
     private:
-        //hay q ver el tipo de datos de la condicion
-        //ejemplo, si guardamos el numero por el cual vemos si es mayor o menor
-        // guardar ese num, y en otro caso guardar el atributo con el cual spliteamos..
+        DataFrame* dataFrame;
+        std::string atributo;
+        std::string categoria;
         double infoGain;
         std::vector<Nodo*>* splits;
 };
