8ddafb5a2ffb03e0f52d2df05b857233ebf242c2
Daniel Sorichetti <me@dancodes.com.ar>
2015-11-21 20:46:55 -0300

    usamos el nuevo csv

--- includes/DataFrame.cpp
+++ includes/DataFrame.cpp
@@ -71,8 +71,9 @@ void DataFrame::leerArchivoTrain() {
     cout << "Abriendo archivo Train" << endl;
 
     //io::CSVReader<9, io::trim_chars<' '>, io::double_quote_escape<',','\"'>> in("data/train.csv");
-    io::CSVReader<9, io::trim_chars<' '>, io::double_quote_escape<',','\"'>> in("data_pruebas/train.25000.csv");
+    //io::CSVReader<9, io::trim_chars<' '>, io::double_quote_escape<',','\"'>> in("data_pruebas/train.25000.csv");
     //io::CSVReader<9, io::trim_chars<' '>, io::double_quote_escape<',','\"'>> in("data_pruebas/train.5.noentropy.csv");
+    io::CSVReader<9, io::trim_chars<' '>, io::double_quote_escape<',','\"'>> in("data_pruebas/train.10.variando.el.PD");
 
     in.read_header(io::ignore_extra_column,"Dates","Category","Descript","DayOfWeek","PdDistrict","Resolution","Address","X","Y");
 
