83f4ab5323942fc038966c31e6c49d652de9b282
Florencia Montero <flopy1706@hotmail.com>
2015-11-25 14:29:31 -0300

    cambios pabadas

--- includes/NAtributo.hpp
+++ includes/NAtributo.hpp
@@ -7,6 +7,7 @@ public:
     NAtributo(std::string nombre);
     std::string obtenerNombre();
     virtual void* obtenerValor() = 0;
+    virtual bool esNumerico();
     virtual ~NAtributo() {
 
     }
@@ -14,10 +15,10 @@ private:
     std::string nombre;
 };
 
-NAtributo::NAtributo(std::string nombre) {
+NAtributo::NAtributo(std::string nombre){
     this->nombre = nombre;
 }
 
-std::string NAtributo::obtenerNombre() {
+std::string NAtributo::obtenerNombre(){
     return this->nombre;
 }
